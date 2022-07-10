dadddb4961e31ed2f3118e19d664bdf100a60034
Igor Khomenko <xomigrek@gmail.com>
2022-07-10 17:36:20 +0300

    Init commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,8 @@
+public class CreditPaymentService {
+    public int calculate (int amount, int period, double percent) {
+        double mounthPercent = percent/12/100;
+        double coefficient = (mounthPercent*Math.pow((1+mounthPercent),period))/(Math.pow((1+mounthPercent),period)-1);
+        int result = (int) (coefficient*amount);
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        System.out.println();
+        System.out.println("Сумма кредита 1 000 000 руб, срок 1 год, процент 9,99%");
+        System.out.println("Ежемесячный платеж "+ service.calculate(1000_000,12, 9.99) + "руб");
+        System.out.println();
+        System.out.println("Сумма кредита 1 000 000 руб, срок 2 года, процент 9,99%");
+        System.out.println("Ежемесячный платеж "+ service.calculate(1000_000,24, 9.99) + "руб");
+        System.out.println();
+        System.out.println("Сумма кредита 1 000 000 руб, срок 3 года, процент 9,99%");
+        System.out.println("Ежемесячный платеж "+ service.calculate(1000_000,36, 9.99) + "руб");
+
+
+
+    }
+}
